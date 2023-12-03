e344e6a3924c5b894f4bc3407da26c8ac2c49a31
Khin Nyein Chan <sshan02001@gmail.com>
2023-12-03 02:31:45 +0000

    add alert,

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,45 +4,63 @@ import TextField from '@mui/material/TextField';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import SaveIcon from '@mui/icons-material/Save';
+import LoadingButton from '@mui/lab/LoadingButton';
+import Alert from '@mui/material/Alert';
+import IconButton from '@mui/material/IconButton';
+import Collapse from '@mui/material/Collapse';
+import CloseIcon from '@mui/icons-material/Close';
+import Swal from 'sweetalert2';
 
-// export default function Form() {
-//   return (
-//     <Stack direction="row" spacing={2}>
-//     <Box sx={{ '& > :not(style)': { m: 1 }, bgcolor: 'white', p:"5px" }}>
-//       <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-//         <AccountCircle sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-//         <TextField id="input-with-sx" label="Receipt Address" variant="standard" />
-
-//         {/* <AccountCircle sx={{ color: 'action.active',  mr: 1, my: 0.5 }} /> */}
-//         <TextField id="input-with-sx" label="Token Amount" type='number' variant="standard" sx={{pl:2}} />
-
-        
-//       </Box>
-//       <Button variant="contained">Sent</Button>
-//     </Box>
-//     </Stack>
-//   );
-// }
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import Web3 from 'web3';
 
 export default function Form() {
   const [recipient, setRecipient] = useState('');
   const [amount, setAmount] = useState('');
   const [transferInProgress, setTransferInProgress] = useState(false);
+  const initialIsPending = localStorage.getItem("pending") === "true";
+  const [isPending, setIsPending] = useState(initialIsPending);
+  const [successAlert,setSuccessAlert] = useState(false)
+  const [errorAlert,setErrorAlert] = useState(false)
 
 
+  useEffect(() => {
+    // Save the current value to local storage whenever it changes
+    localStorage.setItem("pending", isPending.toString());
+  }, [isPending]);
 
+  // Handle local storage changes
+  const handleStorageChange = () => {
+    const updatedIsPending = localStorage.getItem("pending") === "true";
+    setIsPending(updatedIsPending);
+  };
+
+  useEffect(() => {
+    // Add event listener for local storage changes
+    window.addEventListener('storage', handleStorageChange);
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener('storage', handleStorageChange);
+    };
+  }, []); // The empty dependency array ensures that this effect runs only once on mount
+
+console.log(isPending,"ppp");
 
   const initiateTokenTransfer = async () => {
+
+    setErrorAlert(false);
+    setSuccessAlert(false)
     try {
       setTransferInProgress(true); // Set transfer in progress to true
-  
+
+     
       // Check if MetaMask is connected
       if (typeof window.ethereum !== 'undefined') {
         // Use web3.js to interact with the ERC-20 token contract
         const web3 = new Web3(window.ethereum);
-  
+       
         // Call functions to estimate gas and gas price before performing the actual token transfer
         const { gasEstimation, gasPrice } = await estimateGasAndGasPrice(web3, recipient, amount);
   
@@ -54,20 +72,34 @@ export default function Form() {
         const estimatedConfirmationTime = Math.ceil(gasEstimation * gasPrice / 1e10); // Example calculation, adjust as needed
   
         console.log('Estimated Confirmation Time:', estimatedConfirmationTime, 'seconds');
-  
+    
         // Ask the user to confirm the transfer
-        const userConfirmation = window.confirm(`Estimated Gas Cost: ${gasEstimation}\nCurrent Gas Price: ${web3.utils.fromWei(gasPrice, 'gwei')} Gwei\nEstimated Confirmation Time: ${estimatedConfirmationTime} seconds\n\nDo you want to proceed with the token transfer?`);
-  
-        if (userConfirmation) {
-          // Call a function to perform the actual token transfer
-          await performTokenTransfer(web3, recipient, amount);
+      
+        Swal.fire({
+          title: "Do you want to proceed with the token transfer?",
+          html: `<p style="text-align: left;">Estimated Gas Cost:<b> ${gasEstimation}</b></p>
+          <p style="text-align: left;">Current Gas Price: <b>${web3.utils.fromWei(gasPrice, 'gwei')}</b> Gwei</p>
+          <p style="text-align: left;">Estimated Confirmation Time: <b> ${estimatedConfirmationTime} </b>seconds</p>
+          `,
+           icon: "question",
+          showCancelButton: true,
+          confirmButtonColor: "#3085d6",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "Yes, transfer"
+        }).then((result) => {
+          if (result.isConfirmed) {
          
-        } else {
-          console.log('User canceled the transfer.');
-        }
-      } else {
-        console.log('MetaMask not connected. Please connect MetaMask first.');
-      }
+              // Call a function to perform the actual token transfer
+               performTokenTransfer(web3, recipient, amount);
+             
+            } else {
+              console.log('User canceled the transfer.');
+            
+          }
+        });
+      
+  
+    }
     } catch (error) {
       console.error('Error:', error.message);
       // Show an alert for errors
@@ -106,30 +138,37 @@ export default function Form() {
 
   const performTokenTransfer = async (web3, recipient, amount) => {
     try {
+     
       // Validate the recipient address
       if (!web3.utils.isAddress(recipient)) {
         throw new Error('Invalid Ethereum address provided');
       }
-  
+
       // Convert the recipient address to checksum format
+     
       const checksumRecipient = web3.utils.toChecksumAddress(String(recipient));
       console.log(checksumRecipient);
-      // Use web3.js to interact with the ERC-20 token contract
+     
+      // Use web3.js to interact with the ERC-20 oken contract
       const contract = new web3.eth.Contract(contractAbi, contractAddress);
-  
+     
+      setIsPending(true)
       // Perform the token transfer
       const transaction = await contract.methods.transfer(checksumRecipient, amount).send({ from: window.ethereum.selectedAddress });
-  
+      setIsPending(false)
       console.log('Token transfer successful! Transaction Hash:', transaction.transactionHash);
       console.log('Token transfer successful!');
-      window.alert('Transfer successful');
+      setSuccessAlert(true);
+      
     } catch (error) {
+    
+      setIsPending(false)
       // Check if the user canceled the transaction
       if (error.message.includes('User denied transaction signature')) {
         // User canceled the transaction
         console.warn('User canceled the transfer.');
         // You can show an alert or handle it in any other way you prefer
-        window.alert('User canceled the transfer.');
+       setErrorAlert(true)
       } else {
         // Other errors
         console.error('Error performing token transfer:', error.message);
@@ -139,8 +178,9 @@ export default function Form() {
     }
   };
    
-
+ 
   return (
+  <>
     <Stack direction="row" spacing={2}>
     <Box sx={{ '& > :not(style)': { m: 1 }, bgcolor: 'white', p: "5px" }}>
       <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
@@ -150,11 +190,64 @@ export default function Form() {
         <TextField id="input-with-sx" label="Token Amount" type='number' variant="standard" sx={{ pl: 2 }} value={amount} onChange={(e) => setAmount(e.target.value)} />
 
       </Box>
-      <Button onClick={initiateTokenTransfer} variant="contained" disabled={transferInProgress}>
-        {transferInProgress ? 'Pending Transfer' : 'Token Transfer'}
-      </Button>
+    
+        {  isPending ? 
+          <LoadingButton
+        loading
+        loadingPosition="start"
+        startIcon={<SaveIcon />}
+        variant="outlined">
+        Pending
+      </LoadingButton> :  
+       <Button  onClick={initiateTokenTransfer} variant="contained" >Transfer  </Button>
+       }
+   
+    
     </Box>
+  
   </Stack>
+  <Box sx={{ width: '50%' }}>
+      <Collapse in={successAlert}>
+        <Alert severity="success"
+          action={
+            <IconButton
+              aria-label="close"
+              color="inherit"
+              size="small"
+              onClick={() => {
+                setSuccessAlert(false);
+              }}
+            >
+              <CloseIcon fontSize="inherit" />
+            </IconButton>
+          }
+          sx={{ mb: 2 }}
+        >
+          Close me!
+        </Alert>
+      </Collapse>
+      <Collapse in={errorAlert}>
+        <Alert severity="error"
+          action={
+            <IconButton
+              aria-label="close"
+              color="inherit"
+              size="small"
+              onClick={() => {
+                setErrorAlert(false);
+              }}
+            >
+              <CloseIcon fontSize="inherit" />
+            </IconButton>
+          }
+          sx={{ mb: 2 }}
+        >
+          Close me!
+        </Alert>
+      </Collapse>
+    </Box>
+   
+  </>
    
   );
 }
